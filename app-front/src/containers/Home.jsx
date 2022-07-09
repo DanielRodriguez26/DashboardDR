f212a037312b07fec01a4554097d2110991ad260
Daniel Rodriguez <daniel.rodriguez@evolutionxp.com>
2022-07-09 18:53:19 -0500

    back y front integrados

---
+++ app-front/src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import Layout from "../hooks/Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+            <div className="text-blue-500">
+                Home
+            </div>
+        </Layout>
+    )
+};
+
+export default Home;
---
+++ app_back/core/db.py
@@ -0,0 +1,24 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'efficientdrugstore',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
+    }
+}
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommerceDR',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True
+    }
+}
