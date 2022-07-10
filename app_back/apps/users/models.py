72199337afb55ac1cb077859b7868146aad1eee5
Daniel Rodriguez <daniel.rodriguez@evolutionxp.com>
2022-07-10 13:48:12 -0500

    superUser

---
+++ app_back/apps/users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# Create your models here.
+class UserAcountManager(BaseUserManager):
+    def create_user(self,email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self,email, password=None, **extra_fields):
+        user = self.create_user(email, password,**extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
